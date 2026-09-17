9ab5b1b403e7202e9fdaa9809bf7e38ec0fab15a
Michael Elsen <mk05elsen@gmail.com>
2026-09-17 14:30:12 -0500

    Fix constructor declaration formatting in Resource class

--- Resource.h
+++ Resource.h
@@ -8,8 +8,7 @@ using namespace std;
 class Resource {
 public:
     Resource();
-    Resource(const string& id, const string& name,
-             const string& type, bool available);
+    Resource(const string& id, const string& name, const string& type, bool available);
 
     const string& getId() const;
     const string& getName() const;
